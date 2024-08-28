4e93ed3e76c293b4bc9f5c14c4f2cb4c857d31a1
Logan Cole <emtlogancole@gmail.com>
2024-08-28 11:58:50 -0500

    add error handling to NewPostsFromFS and tests

--- blogposts.go
+++ blogposts.go
@@ -9,11 +9,14 @@ type Post struct {
 
 }
 
-func NewPostsFromFS(fileSystem fstest.MapFS) []Post {
-	dir, _ := fs.ReadDir(fileSystem, ".")
+func NewPostsFromFS(fileSystem fstest.MapFS) ([]Post, error ){
+	dir, err := fs.ReadDir(fileSystem, ".")
+	if err != nil {
+		return nil, err
+	}
 	var posts []Post
 	for range dir {
 		posts = append(posts, Post{})
 	}
-	return posts
+	return posts, nil
 }
