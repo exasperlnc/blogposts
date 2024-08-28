026beb9cf9a5c7caf4ed3a1faca5325e2451bf71
Logan Cole <emtlogancole@gmail.com>
2024-08-28 12:09:26 -0500

    test title for Post struct

--- blogposts_test.go
+++ blogposts_test.go
@@ -1,15 +1,17 @@
 package blogposts_test
 
 import (
-	blogposts "github.com/exasperlnc/blogposts"
+	"reflect"
 	"testing"
 	"testing/fstest"
+
+	blogposts "github.com/exasperlnc/blogposts"
 )
 
 func TestNewBlogPosts(t *testing.T) {
 	fs := fstest.MapFS {
-		"hello world.md": {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
+		"hello world.md": {Data: []byte("Title: Post 1")},
+		"hello-world2.md": {Data: []byte("Title: Post 2")},
 	}
 
 	posts, err := blogposts.NewPostsFromFS(fs)
@@ -20,5 +22,12 @@ func TestNewBlogPosts(t *testing.T) {
 
 	if len(posts) != len(fs) {
 		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+
+	got := posts[0]
+	want := blogposts.Post{Title: "Post 1"}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
 	}
 }
