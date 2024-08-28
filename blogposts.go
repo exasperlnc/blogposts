a06726db1d9560dfdea9d77a0db2c7005d587a5b
Logan Cole <emtlogancole@gmail.com>
2024-08-28 14:09:21 -0500

    refactor getPost for SRP

--- blogposts.go
+++ blogposts.go
@@ -3,23 +3,22 @@ package blogposts
 import (
 	"io"
 	"io/fs"
-	"testing/fstest"
 )
 
 type Post struct {
 	Title string
 }
 
-func NewPostsFromFS(fileSystem fstest.MapFS) ([]Post, error ){
+func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	dir, err := fs.ReadDir(fileSystem, ".")
 	if err != nil {
-			return nil, err
-	}		
+		return nil, err
+	}
 	var posts []Post
 	for _, f := range dir {
 		post, err := getPost(fileSystem, f)
 		if err != nil {
-			return nil, err
+			return nil, err 		
 		}
 		posts = append(posts, post)
 	}
@@ -30,14 +29,17 @@ func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
 	postFile, err := fileSystem.Open(f.Name())
 	if err != nil {
 		return Post{}, err
-	}	
+	}
 	defer postFile.Close()
+	return newPost(postFile)
+	}
 
+func newPost(postFile fs.File) (Post, error) {
 	postData, err := io.ReadAll(postFile)
 	if err != nil {
 		return Post{}, err
-	}	
+	}
 
-	post := Post{Title: string(postData)[7:]}
+	post := Post{Title:string(postData)[7:]}
 	return post, nil
 }
